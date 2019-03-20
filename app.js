0752706ad9929d0e477076638931cb95faf708ac
Mustafa Sohail <mustafasohail07@gmail.com>
2019-03-20 00:25:38 -0500

    Added passport auth

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-const express = require('express'),
-    bodyParser = require('body-parser'),
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     app = express(),
+    User = require("./models/user"),
+    indexRoutes = require("./routes/index"),
+    userRoutes = require("./routes/users")
     port = 3000;
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-app.get('/', (req, res) => res.render("home"));
 
-// app.use("/", indexRoutes);
+mongoose.connect("mongodb://localhost/gitsprint", {useNewUrlParser: true});
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "SecretHashKey",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate())); // User.authenticate() method comes with passportLocalMongoose
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use("/", indexRoutes);
+app.use("/users", userRoutes);
 // app.use("/campgrounds/:id/comments", commentRoutes);
 // app.use("/campgrounds", campgroundRoutes);
 
